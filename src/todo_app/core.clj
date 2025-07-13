83b0562f0106c46aa84c766e22be47feec48a303
Nathan Yam <nathancyam@gmail.com>
2025-07-13 16:46:44 +1000

    Add core.async to handle shutdown

--- src/todo_app/core.clj
+++ src/todo_app/core.clj
@@ -1,8 +1,22 @@
 (ns todo-app.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [todo-app.handler :refer [app]]))
+            [todo-app.handler :refer [app]]
+            [clojure.core.async :refer [go <!! >!! chan]]))
+
+(defonce signal (chan))
 
 (defn -main [& args]
   (let [port (Integer/parseInt (or (System/getenv "PORT") "3000"))]
     (println (str "Starting server on port " port))
-    (run-jetty app {:port port :join? false})))
+    (.addShutdownHook
+     (Runtime/getRuntime)
+     (Thread. (fn []
+                (println "Sending exit signal")
+                (>!! signal :shutdown))))
+    (let [server (run-jetty app {:port port :join? false})]
+      (go
+        (println "Started server on port " port " and waiting for shutdown signal...")
+        (<!! signal)
+        (println "Shutting down server...")
+        (.stop server)
+        (println "Server stopped.")))))
