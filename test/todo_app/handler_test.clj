ff2b4df3099b1eb8b603fbd15df5704ce27c7ec6
Nathan Yam <nathancyam@gmail.com>
2025-12-01 23:17:36 +1100

    Add db tests

--- test/todo_app/handler_test.clj
+++ test/todo_app/handler_test.clj
@@ -1,6 +1,38 @@
 (ns todo-app.handler-test
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is use-fixtures]]
+            [ring.mock.request :as mock]
+            [next.jdbc.connection :as connection]
+            [ragtime.next-jdbc :as ragtime-jdbc]
+            [ragtime.repl :as ragtime-repl]
+            [todo-app.handler :refer [routes]])
+  (:import (com.zaxxer.hikari HikariDataSource)))
+
+(def ^:dynamic *db* nil)
+
+(def db-spec
+  {:dbtype "postgres"
+   :dbname "todoapp_test"
+   :host "localhost"
+   :username "postgres"
+   :password "postgres"})
+
+(defn setup-db [f]
+  (let [ds (connection/->pool HikariDataSource db-spec)]
+    (try
+      (binding [*db* ds]
+        ;; Run migrations/schema setup
+        (ragtime-repl/migrate {:datastore (ragtime-jdbc/sql-database ds)
+                               :migrations (ragtime-jdbc/load-resources "migrations")})
+        (f))
+      (finally
+        (.close ds)))))
+
+(use-fixtures :once setup-db)
 
 (deftest test-additions
   (is (= (+ 1 2) 3))
   (is (= (+ -1 1) 0)))
+
+(deftest test-todo-app-id-routes
+  (let [response (routes (mock/request :post "/todo" {:title "Title"}))]
+    (is (= 201 (:status response)))))
