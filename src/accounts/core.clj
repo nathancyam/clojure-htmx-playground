b17f02d0489c3583cbd9ba1fe42043b91ca037f6
Nathan Yam <nathancyam@gmail.com>
2025-12-06 20:07:41 +1100

    Add functioning login page

--- src/accounts/core.clj
+++ src/accounts/core.clj
@@ -1,7 +1,6 @@
 (ns accounts.core
   (:require [honey.sql :as sql]
             [honey.sql.helpers :refer [select from where insert-into values returning order-by]]
-            [storage.db :refer [get-db]]
             [crypto.password.bcrypt :as password]
             [next.jdbc :as jdbc]))
 
@@ -20,22 +19,20 @@
                   sql/format)]
     (first (jdbc/execute! db query))))
 
-(defn get-all-users
-  []
-  (let [query (-> (select :*)
-                  (from :users)
-                  (order-by [:inserted_at :desc])
-                  sql/format)]
-    (jdbc/execute! (get-db) query)))
-
 (defn get-user-by-email
   "Get a user by their email address."
-  [email]
+  [db email]
   (let [query (-> (select :*)
                   (from :users)
                   (where [:= :email email])
                   sql/format)]
-    (first (jdbc/execute! (get-db) query))))
+    (first (jdbc/execute! db query))))
+
+(defn authenticate [db email password]
+  (if-let [result (get-user-by-email db email)]
+    (when (password/check password (:users/hashed_passwod result))
+      result)
+    (throw (Exception. "user not found"))))
 
 (defn create-user! [db user-data]
   (let [query (-> (insert-into :users)
--- src/accounts/web.clj
+++ src/accounts/web.clj
@@ -1,30 +1,40 @@
 (ns accounts.web
   (:require
+   [accounts.core :as a]
    [compojure.core :refer [defroutes GET POST]]
    [views.components :refer [csrf-token]]
-   [views.response :refer [page-response]]))
+   [views.response :refer [hx-response page-response]]))
 
-(defn login-page []
+(defn- login-form
+  [form errors]
+  [:form {:hx-post "/accounts/login" :hx-target "#main-wrapper" :hx-swap "outerHTML" :class "flex flex-col gap-4"}
+   (csrf-token)
+   [:div
+    [:label {:for "email" :class "block text-gray-700 mb-2"} "Username"]
+    [:input {:type "text" :name "email" :value (:email form) :id "email" :class "w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"}]]
+   [:div
+    [:label {:for "password" :class "block text-gray-700 mb-2"} "Password"]
+    [:input {:type "password" :name "password" :id "password" :class "w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"}]]
+   [:div
+    (for [error errors]
+      [:p {:class "text-red-500 text-sm mb-2"} error])]
+   [:button {:type "submit" :class "w-full bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg transition-colors"} "Login"]])
+
+(defn login-page
+  [form errors]
   [:div {:id "login-page" :class "max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md"}
    [:h2 {:class "text-2xl font-bold mb-4 text-center"} "Login"]
-   [:form {:hx-post "/accounts/login" :hx-target "#main-wrapper" :hx-swap "outerHTML"}
-    (csrf-token)
-    [:div {:class "mb-4"}
-     [:label {:for "username" :class "block text-gray-700 mb-2"} "Username"]
-     [:input {:type "text" :name "username" :id "username" :class "w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"}]]
-    [:div {:class "mb-6"}
-     [:label {:for "password" :class "block text-gray-700 mb-2"} "Password"]
-     [:input {:type "password" :name "password" :id "password" :class "w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"}]]
-    [:button {:type "submit" :class "w-full bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg transition-colors"} "Login"]]])
+   (login-form form errors)])
 
 (defroutes routes
-  (POST "/login" [username password]
-    (prn username)
-    (prn password)
-    (page-response [:div {:class "max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md"}
-                    [:h2 {:class "text-2xl font-bold mb-4 text-center"} "Login Successful"]
-                    [:p {:class "text-center text-gray-700"} "Welcome back!"]]))
+  (POST "/login" [email password :as {db :db}]
+    (try
+      (prn (a/authenticate db email password))
+      (page-response [:div {:class "max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md"}
+                      [:h2 {:class "text-2xl font-bold mb-4 text-center"} "Login Successful"]
+                      [:p {:class "text-center text-gray-700"} "Welcome back!"]])
+      (catch Exception _
+        (hx-response (login-page {:email email} ["Invalid user or password"])))))
 
-  (GET "/login" request
-    (prn request)
-    (page-response (login-page))))
+  (GET "/login" []
+    (page-response (login-page {} []))))
